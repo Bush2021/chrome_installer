b55c7bc524237c9d3dfb8378319c9cf3bb72ec55
shuax <admin@shuax.com>
2022-05-14 10:38:59 +0800

    自动抓取

---
+++ fetch.py
@@ -0,0 +1,130 @@
+import requests
+import xml.etree.ElementTree as tree
+import base64
+import binascii
+import json
+from datetime import datetime, timezone
+
+info = {
+    "win_stable_x86": {
+        "os": '''platform="win" version="6.3" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="-multi-chrome"''',
+    },
+    "win_stable_x64": {
+        "os": '''platform="win" version="6.3" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-stable-multi-chrome"''',
+    },
+    "win_beta_x86": {
+        "os": '''platform="win" version="6.3" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="1.1-beta"''',
+    },
+    "win_beta_x64": {
+        "os": '''platform="win" version="6.3" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-beta-multi-chrome"''',
+    },
+    "win_dev_x86": {
+        "os": '''platform="win" version="6.3" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="2.0-dev"''',
+    },
+    "win_dev_x64": {
+        "os": '''platform="win" version="6.3" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-dev-multi-chrome"''',
+    },
+    "win_canary_x86": {
+        "os": '''platform="win" version="6.3" arch="x86"''',
+        "app": '''appid="{4EA16AC7-FD5A-47C3-875B-DBF4A2008C20}" ap=""''',
+    },
+    "win_canary_x64": {
+        "os": '''platform="win" version="6.3" arch="x64"''',
+        "app": '''appid="{4EA16AC7-FD5A-47C3-875B-DBF4A2008C20}" ap="x64-canary"''',
+    },
+    "mac_stable": {
+        "os": '''platform="mac" version="46.0.2490.86" arch="x64"''',
+        "app": '''appid="com.google.Chrome" ap=""''',
+    },
+    "mac_beta": {
+        "os": '''platform="mac" version="46.0.2490.86" arch="x64"''',
+        "app": '''appid="com.google.Chrome" ap="betachannel"''',
+    },
+    "mac_dev": {
+        "os": '''platform="mac" version="46.0.2490.86" arch="x64"''',
+        "app": '''appid="com.google.Chrome" ap="devchannel"''',
+    },
+    "mac_canary": {
+        "os": '''platform="mac" version="46.0.2490.86" arch="x64"''',
+        "app": '''appid="com.google.Chrome.Canary" ap=""''',
+    },
+}
+
+update_url = 'http://tools.google.com/service/update2'
+
+
+def post(os, app):
+    xml = '''<?xml version="1.0" encoding="UTF-8"?>
+    <request protocol="3.0" version="1.3.23.9" ismachine="0">
+    <hw sse="1" sse2="1" sse3="1" ssse3="1" sse41="1" sse42="1" avx="1" physmemory="12582912" />
+    <os {0}/>
+    <app {1}>
+    <updatecheck/>
+    </app>
+    </request>'''.format(os, app)
+    # print(xml)
+    r= requests.post(update_url, data=xml)
+    return r.text
+
+def decode(text):
+
+    root = tree.fromstring(text)
+
+    manifest_node = root.find('.//manifest')
+    manifest_version = manifest_node.get('version')
+
+    package_node = root.find('.//package')
+    package_name = package_node.get('name')
+    package_size = int(package_node.get('size'))
+    package_sha1 = package_node.get('hash')
+    package_sha1 = base64.b64decode(package_sha1)
+    package_sha1 = package_sha1.hex().upper()
+    package_sha256 = package_node.get('hash_sha256').upper()
+
+    url_nodes = root.findall('.//url')
+
+    url_prefixes = []
+    for node in url_nodes:
+        url_prefixes.append(node.get('codebase') + package_name)
+
+    return {"version":manifest_version, "size":package_size, "sha1":package_sha1, "sha256":package_sha256, "urls":url_prefixes}
+
+results = {}
+def fetch():
+    for k, v in info.items():
+        t = post(**v)
+        results[k] = decode(t)
+
+def save_md():
+    with open('readme.md', 'w') as f:
+        f.write(f'# Automatic Generated Time\n')
+        f.write(f'{datetime.now(timezone.utc)}\n')
+        f.write('\n')
+        for k, v in results.items():
+            f.write(f'# {k}\n')
+            f.write(f'version:{v["version"]}\n')
+            f.write(f'size:{v["size"]}\n')
+            f.write(f'sha1:{v["sha1"]}\n')
+            f.write(f'sha256:{v["sha256"]}\n')
+            for url in v["urls"]:
+                if url.startswith("https://dl."):
+                    f.write(f'download:[{url}]({url})\n')
+
+            f.write('\n')
+
+def save_json():
+    with open('data.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+def main():
+    fetch()
+    save_md()
+    save_json()
+
+main()
