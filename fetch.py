64f84d7b7465b7e9ff39a4f49034252f8414dd95
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2024-02-16 20:34:15 -0500

    chore: initialize repository

---
+++ fetch.py
@@ -0,0 +1,160 @@
+import base64
+import json
+import xml.etree.ElementTree as tree
+from datetime import datetime, timezone, timedelta
+
+import requests
+
+info = {
+    "win_stable_x86": {
+        "os": '''platform="win" version="10.0" sp="" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" version="" nextversion="" lang="en" brand=""  installage="-1" installdate="-1" iid="{11111111-1111-1111-1111-111111111111}"''',
+    },
+    "win_stable_x64": {
+        "os": '''platform="win" version="10.0" sp="" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" version="" nextversion="" lang="en" brand=""  installage="-1" installdate="-1" iid="{11111111-1111-1111-1111-111111111111}"''',
+    },
+    "win_beta_x86": {
+        "os": '''platform="win" version="10.0" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="1.1-beta"''',
+    },
+    "win_beta_x64": {
+        "os": '''platform="win" version="10.0" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-beta-multi-chrome"''',
+    },
+    "win_dev_x86": {
+        "os": '''platform="win" version="10.0" arch="x86"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="2.0-dev"''',
+    },
+    "win_dev_x64": {
+        "os": '''platform="win" version="10.0" arch="x64"''',
+        "app": '''appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-dev-multi-chrome"''',
+    },
+    "win_canary_x86": {
+        "os": '''platform="win" version="10.0" arch="x86"''',
+        "app": '''appid="{4EA16AC7-FD5A-47C3-875B-DBF4A2008C20}" ap="x86-canary"''',
+    },
+    "win_canary_x64": {
+        "os": '''platform="win" version="10.0" arch="x64"''',
+        "app": '''appid="{4EA16AC7-FD5A-47C3-875B-DBF4A2008C20}" ap="x64-canary"''',
+    },
+}
+
+update_url = 'https://tools.google.com/service/update2'
+
+session = requests.Session()
+
+
+def post(os: str, app: str) -> str:
+    xml = f'''<?xml version="1.0" encoding="UTF-8"?>
+    <request protocol="3.0" updater="Omaha" updaterversion="1.3.36.372" shell_version="1.3.36.352" ismachine="0" sessionid="{11111111-1111-1111-1111-111111111111}" installsource="taggedmi" requestid="{11111111-1111-1111-1111-111111111111}" dedup="cr" domainjoined="0">
+    <hw physmemory="16" sse="1" sse2="1" sse3="1" ssse3="1" sse41="1" sse42="1" avx="1"/>
+    <os {os}/>
+    <app {app}>
+    <updatecheck/>
+    <data name="install" index="empty"/>
+    </app>
+    </request>'''
+    r = session.post(update_url, data=xml)
+    return r.text
+
+
+def decode(text):
+    root = tree.fromstring(text)
+
+    manifest_node = root.find('.//manifest')
+    manifest_version = manifest_node.get('version')
+
+    package_node = root.find('.//package')
+    package_name = package_node.get('name')
+    package_size = int(package_node.get('size'))
+    package_sha1 = package_node.get('hash')
+    package_sha1 = base64.b64decode(package_sha1)
+    package_sha1 = package_sha1.hex()
+    package_sha256 = package_node.get('hash_sha256')
+
+    url_nodes = root.findall('.//url')
+
+    url_prefixes = []
+    for node in url_nodes:
+        url_prefixes.append(node.get('codebase') + package_name)
+
+    return {"version": manifest_version, "size": package_size, "sha1": package_sha1, "sha256": package_sha256,
+            "urls": url_prefixes}
+
+
+results = {}
+
+
+def version_tuple(v):
+    return tuple(map(int, (v.split("."))))
+
+
+def load_json() -> None:
+    global results
+    with open('data.json', 'r') as f:
+        results = json.load(f)
+
+
+def fetch():
+    for k, v in info.items():
+        res = post(**v)
+        data = decode(res)
+        if version_tuple(data['version']) < version_tuple(results[k]['version']):
+            print("ignore", k, data['version'])
+            continue
+        results[k] = data
+
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+
+def humansize(nbytes):
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes) - 1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+
+def save_md() -> None:
+    index_url = "https://github.com/Bush2021/chrome_installer?tab=readme-ov-file#"
+    with open('readme.md', 'w') as f:
+        f.write(f'# Google Chrome 离线安装包（请使用 7-Zip 解压）\n')
+        f.write(f'稳定版存档：<https://github.com/Bush2021/chrome_installer/releases>\n\n')
+        f.write(f'最后检测更新时间\n')
+        now = datetime.now(timezone(timedelta(hours=-5)))
+        now_str = now.strftime("%Y-%m-%d %H:%M:%S (UTC-5)")
+        f.write(f'{now_str}\n\n')
+        f.write('\n')
+        f.write(f'## 目录\n')
+        for name in results.keys():
+            title = name[7:].replace("win-", "").replace("-", " ")
+            link = index_url + title.replace(" ", "-")
+            f.write(f'* [{title}]({link})\n')
+        f.write('\n')
+        for k, v in results.items():
+            f.write(f'## {k.replace("_", " ")}\n')
+            f.write(f'**最新版本**：{v["version"]}  \n')
+            f.write(f'**文件大小**：{humansize(v["size"])}  \n')
+            f.write(f'**校验值（Sha256）**：{v["sha256"]}  \n')
+            for url in v["urls"]:
+                if url.startswith("https://dl."):
+                    f.write(f'**下载链接**：[{url}]({url})  \n')
+            f.write('\n')
+
+
+def save_json():
+    with open('data.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+
+def main() -> None:
+    load_json()
+    fetch()
+    save_md()
+    save_json()
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Google Chrome 离线安装包（请使用 7-Zip 解压）
+最后检测更新时间
+2024-01-12 19:36:40 (UTC-5)
+
+## win stable x86
+**最新版本**：120.0.6099.217  
+**文件大小**：88.25 MB  
+**校验值（Sha256）**：1c9fecb3245aa3d41a4600aaaf28e04f16727cf21941a69422e25b9114096956  
+**下载链接**：[https://dl.google.com/release2/chrome/kcjdv7hmimly2olpybynchk4jy_120.0.6099.217/120.0.6099.217_chrome_installer.exe](https://dl.google.com/release2/chrome/kcjdv7hmimly2olpybynchk4jy_120.0.6099.217/120.0.6099.217_chrome_installer.exe)  
+
+## win stable x64
+**最新版本**：120.0.6099.217  
+**文件大小**：106.33 MB  
+**校验值（Sha256）**：0d70b4acc3da57b1e89dcff25b7ae037248d7cfdb8ba3ba06cf6a6644a013414  
+**下载链接**：[https://dl.google.com/release2/chrome/kiwlingl36y4sj7nxjd2a564xa_120.0.6099.217/120.0.6099.217_chrome_installer.exe](https://dl.google.com/release2/chrome/kiwlingl36y4sj7nxjd2a564xa_120.0.6099.217/120.0.6099.217_chrome_installer.exe)  
+
+## win beta x86
+**最新版本**：121.0.6167.57  
+**文件大小**：96.1 MB  
+**校验值（Sha256）**：5922b057f1c1d5e125c12e2fce4faeba948d5ca36e1d799e630926920cdc8ae0  
+**下载链接**：[https://dl.google.com/release2/chrome/bcolxw3r3c2extnisarwb7vysq_121.0.6167.57/121.0.6167.57_chrome_installer.exe](https://dl.google.com/release2/chrome/bcolxw3r3c2extnisarwb7vysq_121.0.6167.57/121.0.6167.57_chrome_installer.exe)  
+
+## win beta x64
+**最新版本**：121.0.6167.57  
+**文件大小**：107.77 MB  
+**校验值（Sha256）**：33eb374f62b2573c7e0331bb8bb8305b8b250b5b19585691999898f9b5016b32  
+**下载链接**：[https://dl.google.com/release2/chrome/adeywxndo6coqo7ciaydfzwnnniq_121.0.6167.57/121.0.6167.57_chrome_installer.exe](https://dl.google.com/release2/chrome/adeywxndo6coqo7ciaydfzwnnniq_121.0.6167.57/121.0.6167.57_chrome_installer.exe)  
+
+## win dev x86
+**最新版本**：122.0.6238.2  
+**文件大小**：96.27 MB  
+**校验值（Sha256）**：b66c1ccb30b339f64c1848ad44c74ae7c5c254776df51bde0abe2a0bcbeb9648  
+**下载链接**：[https://dl.google.com/release2/chrome/accupmzmrxjhuawceitpkznzuioa_122.0.6238.2/122.0.6238.2_chrome_installer.exe](https://dl.google.com/release2/chrome/accupmzmrxjhuawceitpkznzuioa_122.0.6238.2/122.0.6238.2_chrome_installer.exe)  
+
+## win dev x64
+**最新版本**：122.0.6238.2  
+**文件大小**：107.82 MB  
+**校验值（Sha256）**：01cf2656b08a82e96531b4b4f37a7e80ca29540687af4f4a46e83bfa2f3000f9  
+**下载链接**：[https://dl.google.com/release2/chrome/acdwptafyk3tkqepnusymgjofghq_122.0.6238.2/122.0.6238.2_chrome_installer.exe](https://dl.google.com/release2/chrome/acdwptafyk3tkqepnusymgjofghq_122.0.6238.2/122.0.6238.2_chrome_installer.exe)  
+
+## win canary x86
+**最新版本**：122.0.6241.5  
+**文件大小**：96.33 MB  
+**校验值（Sha256）**：6f0f1b8facd88c33813f62854fdecb2da1c39ddbb97dc66307ddc1216b9c97a0  
+**下载链接**：[https://dl.google.com/release2/chrome/nef7bkldqlrwlsjdss6yrrdeqq_122.0.6241.5/122.0.6241.5_chrome_installer.exe](https://dl.google.com/release2/chrome/nef7bkldqlrwlsjdss6yrrdeqq_122.0.6241.5/122.0.6241.5_chrome_installer.exe)  
+
+## win canary x64
+**最新版本**：122.0.6241.5  
+**文件大小**：107.9 MB  
+**校验值（Sha256）**：579aa2552305f725d935a2be62f7369db0122b27f4969b29914393f8c4f6cfdd  
+**下载链接**：[https://dl.google.com/release2/chrome/i5iv5ooabe7xqjfw3z25efasoy_122.0.6241.5/122.0.6241.5_chrome_installer.exe](https://dl.google.com/release2/chrome/i5iv5ooabe7xqjfw3z25efasoy_122.0.6241.5/122.0.6241.5_chrome_installer.exe)  
+
